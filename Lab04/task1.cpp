13c9aaef7e07eda4f9a10ea91e18eba6b3fc0c34
msaadsohail05 <msaad.bahria@gmail.com>
2025-09-14 13:15:47 +0500

    Update task1.cpp

--- Lab04/task1.cpp
+++ Lab04/task1.cpp
@@ -1,1 +1,29 @@
-// Muhammad Saad Sohail    24K-0549
+//Muhammad Saad Sohail    24K-0549
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+	int n,flag =0,search;
+	cout<<"Enter the size of the array: ";
+	cin>>n;
+	int array1[n];
+	for(int i=0;i<n;i++){
+		cout<<"Enter the value for index "<<i<<": ";
+		cin>>array1[i];
+	}
+	cout<<"Enter the value to be searched: ";
+	cin>>search;
+	for(int j=0;j<n;j++){
+		if(array1[j] == search){
+			cout<<"Value found at position: "<<j<<endl;
+			flag = 1;
+		}
+	}
+	if(flag == 0){
+		cout<<"Search value is not present inside the array"<<endl;
+	}
+	
+}
